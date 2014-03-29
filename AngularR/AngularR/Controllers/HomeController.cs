de2935ffd1ca585da601f116b6a5302024ee2cfe
hallizh <hallizh1@gmail.com>
2014-03-29 00:28:21 +0000

    Hub working

--- AngularR/AngularR/Controllers/HomeController.cs
+++ AngularR/AngularR/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace AngularR.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult HubTest()
+        {
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
---
+++ AngularR/AngularR/Hubs/Anghub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AngularR.Hubs
+{
+    public class Anghub : Hub
+    {
+
+        public void Send(string name)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name);
+        }
+        
+    }
+}
